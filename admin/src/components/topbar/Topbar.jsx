e18c4764e07c5b22b7e19d088f4b4c4726f2852e
Sneha <81133168+snehabhamra@users.noreply.github.com>
2022-01-16 09:48:42 +1100

    further styling to admin topbar

--- admin/src/components/topbar/Topbar.jsx
+++ admin/src/components/topbar/Topbar.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from 'react';
+import "./topbar.css";
+import {NotificationsNone, Language, Settings} from '@material-ui/icons';
 
 export default function Topbar() {
   return (
-    <div>
-      
+    <div className="topbar">
+      <div className="topbarWrapper">
+        <div className="topLeft">
+          <span className="logo">Soot Flowcraft Admin</span>
+        </div>
+        <div className="topRight">
+          <div className="topbarIconContainer">
+            <NotificationsNone />
+            <span className="topIconBadge">2</span>
+          </div>
+          <div className="topbarIconContainer">
+            <Language />
+            <span className="topIconBadge">2</span>
+          </div>
+          <div className="topbarIconContainer">
+            <Settings />
+          </div>
+          {/* logo image to be linked below */}
+          <img src="../admin/public/soot01.jpg" alt="" className="topAvatar" />
+        </div>
+      </div>
     </div>
   )
 }
