7ce50d8dd82e5d6ea255e148b8b396452c979a3c
Sneha <81133168+snehabhamra@users.noreply.github.com>
2022-01-16 20:08:31 +1100

    added chart component

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,7 +1,7 @@
 import Topbar from "../src/components/topbar/Topbar";
 import Sidebar from "../src/components/sidebar/Sidebar";
 import "./app.css";
-
+import Home from "./pages/home/Home";
 
 function App() {
   return (
@@ -9,7 +9,7 @@ function App() {
     <Topbar />
     <div className="container">
       <Sidebar />
-      <div className="others">other pages</div>
+      <Home />
     </div>
   </div>
   );
