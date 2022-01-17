b1149960cd814a157109dabed89fc59faa7353a6
Sneha <81133168+snehabhamra@users.noreply.github.com>
2022-01-17 17:43:02 +1100

    modified productlist

--- admin/src/pages/products/ProductList.jsx
+++ admin/src/pages/products/ProductList.jsx
@@ -29,7 +29,7 @@ export default function ProductList() {
         },
         {
             field: 'name',
-            headerName: 'Name',
+            headerName: 'name',
             width: 200,
         },
 
