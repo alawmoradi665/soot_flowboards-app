8dc633f1b53df2a7b3bca8e72c147fbea9de4f61
Sneha <81133168+snehabhamra@users.noreply.github.com>
2022-01-17 12:07:17 +1100

    added react router dom 5.2.0 for routing

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,17 +1,31 @@
 import Topbar from "../src/components/topbar/Topbar";
 import Sidebar from "../src/components/sidebar/Sidebar";
 import "./app.css";
+import UserList from "./pages/userList/UserList";
 import Home from "./pages/home/Home";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
 
 function App() {
   return (
-  <div>
+  <Router>
     <Topbar />
     <div className="container">
       <Sidebar />
-      <Home />
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+        <Route path="/users">
+          <UserList />
+        </Route>
+      </Switch>
     </div>
-  </div>
+  </Router>
   );
 }
 
