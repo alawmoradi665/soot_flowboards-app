657d8c297c6d82a7628d5280c649fc5143fbdf65
Sneha <81133168+snehabhamra@users.noreply.github.com>
2022-01-17 21:04:58 +1100

    trying to set up login so that only logged in admin can see dashboard components

--- admin/src/App.js
+++ admin/src/App.js
@@ -10,40 +10,50 @@ import ProductList from "./pages/products/ProductList";
 import Orders from "./pages/orders/Orders";
 import Product from "./pages/product/Product";
 import NewProduct from "./pages/newProduct/NewProduct";
+import Login from "./pages/login/Login";
 
 function App() {
+
+  const admin = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.isAdmin;
   return (
     <Router>
-      <Topbar />
-      <div className="container">
-        <Sidebar />
         <Switch>
-          <Route exact path="/">
-            <Home />
+          <Route path="/Login">
+            <Login />
           </Route>
-          <Route path="/users">
-            <UserList />
-          </Route>
-          <Route path="/user/:userId">
-            <User />
-          </Route>
-          <Route path="/newUser">
-            <NewUser />
-          </Route>
-          <Route path="/products">
-            <ProductList />
-          </Route>
-          <Route path="/product/:productId">
-            <Product />
-          </Route>
-          <Route path="/newproduct">
-            <NewProduct />
-          </Route>
-          <Route path="/orders">
-            <Orders />
-          </Route>
+          {admin && (
+            <>
+            <Topbar />
+            <div className="container">
+              <Sidebar />
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route path="/users">
+                <UserList />
+              </Route>
+              <Route path="/user/:userId">
+                <User />
+              </Route>
+              <Route path="/newUser">
+                <NewUser />
+              </Route>
+              <Route path="/products">
+                <ProductList />
+              </Route>
+              <Route path="/product/:productId">
+                <Product />
+              </Route>
+              <Route path="/newproduct">
+                <NewProduct />
+              </Route>
+              <Route path="/orders">
+                <Orders />
+              </Route>
+            </div>
+            </>)
+          }
         </Switch>
-      </div>
     </Router>
   );
 }
